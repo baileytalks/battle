4c53b5cec2316306eb645bbfebc38f3b094806e0
Bailey Kursar <bkursar@gmail.com>
2017-04-26 19:56:14 +0100

    A first stab at a fight button and fight page

--- app.rb
+++ app.rb
@@ -11,13 +11,25 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
-    redirect to('/play') 
+    session[:hp_player1] = rand(6)+15
+    session[:hp_player2] = rand(6)+15
+    redirect to('/play')
     end
 
   get '/play' do
     @player1 = session[:player1]
     @player2 = session[:player2]
+    @hp_player1 = session[:hp_player1]
+    @hp_player2 = session[:hp_player2]
     erb(:play)
+  end
+
+  get '/fight' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    @hp_player1 = session[:hp_player1]
+    @hp_player2 = session[:hp_player2]
+    erb(:fight)
   end
 
   run! if app_file == $0
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -7,13 +7,23 @@ feature do
     expect(page).to have_content('Testing infrastructure working!')
   end
 
-  scenario 'when a person adds their name, see that name on the screen' do
+  scenario 'when player 1 is playing, they need to see player 2 HP count' do
     visit('/')
     fill_in('player1', with: 'Gordon')
     fill_in('player2', with: 'Foo')
     click_button 'Create Players'
-    expect(page).to have_text('Player 1 is Gordon')
-    expect(page).to have_text('Player 2 is Foo')
+    expect(page).to have_text('Player 1 is Gordon, who has')
+    expect(page).to have_text('Player 2 is Foo, who has')
     expect(page).to have_text('Let the battle commence!')
+  end
+
+  scenario 'when you hit the fight button, you start the fight and see HP count' do
+    visit('/')
+    fill_in('player1', with: 'Gordon')
+    fill_in('player2', with: 'Foo')
+    click_button 'Create Players'
+    click_button 'Fight!'
+    expect(page).to have_text('Gordon')
+    expect(page).to have_text('HP:')
   end
 end
